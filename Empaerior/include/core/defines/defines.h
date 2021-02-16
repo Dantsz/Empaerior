3842338a7ecd3a131a0d05d36f0837f56bafd0e9
Dan Costinas <costinasdan35@yahoo.com>
2021-02-16 13:51:39 +0200

    Renoved defines macros

--- Empaerior/include/core/defines/defines.h
+++ Empaerior/include/core/defines/defines.h
@@ -4,29 +4,3 @@
 #include "basic_defines.h"
 #include "struct_defines.h"
 
-
-
-
-//Platform detection
-#ifdef _WIN32
-	#ifdef _WIN64//if 64 bit 
-	//add 64 flag
-		#define EMPAERIOR_64BIT
-	#else // if 32 bit
-		#define EMPAERIOR_32BIT
-		#pragma warning  "x86 is not a actively tested"	//x86 flag
-	
-#endif // _WIN64
-
-#elif __APPLE__
-	#error "No Apple platforms are supported"
-#elif __linux__
-	#define EMPAERIOR_64BIT
-#else
-	#error "platform not supported yet"
-
-#endif 
-
-#if defined(EMPAERIOR_DEBUG) || defined(EMPAERIOR_RELEASE)
-#define EMP_USE_DATA_DUMPS
-#endif
