ce70b3847e447bbcab8f13ce8434cbc9c622e281
Dan Costinas <costinasdan35@yahoo.com>
2021-11-16 08:42:11 +0200

    bit cast tests

--- Empaerior/src/rendering/vulkan_rendering/renderer.cpp
+++ Empaerior/src/rendering/vulkan_rendering/renderer.cpp
@@ -299,7 +299,7 @@ static VkShaderModule createShaderModule(VkDevice& device, const std::vector<cha
     VkShaderModuleCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
     createInfo.codeSize = code.size();
-    createInfo.pCode = std::bit_cast<const uint32_t*>(code.data());
+    createInfo.pCode = (const uint32_t*)(code.data());
 
     VkShaderModule shaderModule{};
     if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
